933be680faf7406ad95de0550c242cd3abd2817b
ArhamJamil <MUHAMMADARHAMJAMILSABIH@gmail.com>
2024-06-04 00:20:40 +0500

    form card

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-
+import SignUp from "./signup/page";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-slate-400">
-        <h1 className="text-3xl font-bold underline p-3 text-blue-500">
-          Hello world!
-        </h1>
-     
+      <div>
+       <SignUp></SignUp>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{ts,tsxjs,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
